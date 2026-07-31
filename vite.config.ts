98cc5cf7e2f207ff8ef6e4c3d538b529f78dd2ba
notfrontendreact <notfrontendreact@gmail.com>
2026-08-01 00:24:53 +0330

    setup github pages and actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  // این خط را اضافه کنید تا مسیر پروژه در گیت‌هاب پیجیز درست کار کند
+  base: '/daftar/', 
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };  
+  },
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+  },
 });
